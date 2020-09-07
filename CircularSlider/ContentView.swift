e2f66e16b848b4ae64c1c9faf667c93f0e167642
George Davis IV <gdavisiv@gmail.com>
2020-09-06 22:11:13 -0400

    UIUpdate

--- CircularSlider/ContentView.swift
+++ CircularSlider/ContentView.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView{
+            Home()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    
+    @State var size = UIScreen.main.bounds.width - 100
+    
+    var body: some View {
+        VStack{
+            ZStack{
+                Circle()
+                    .stroke(Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)), style: StrokeStyle(lineWidth: 55, lineCap: .round, lineJoin: .round))
+                    .frame(width: size, height: size)
+            }
+        }
     }
 }
