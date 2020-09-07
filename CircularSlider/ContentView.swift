788038637d50dd6c5a649698a34f0c843516fb26
George Davis IV <gdavisiv@gmail.com>
2020-09-06 22:16:08 -0400

    UIUpdates

--- CircularSlider/ContentView.swift
+++ CircularSlider/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             Home()
+                .preferredColorScheme(.dark)
+                .navigationTitle("Wallet")
         }
     }
 }
