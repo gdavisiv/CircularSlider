45d50de7a7c1a0fe8082156f980677aa2da5c3c8
George Davis IV <gdavisiv@gmail.com>
2020-09-07 22:59:09 -0400

    Created Drag Button

--- CircularSlider/ContentView.swift
+++ CircularSlider/ContentView.swift
@@ -33,6 +33,10 @@ struct Home: View {
                 Circle()
                     .stroke(Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)), style: StrokeStyle(lineWidth: 55, lineCap: .round, lineJoin: .round))
                     .frame(width: size, height: size)
+                Circle()
+                    .fill(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    .frame(width: 55, height: 55)
+                    .offset(x: size / 2)
             }
         }
     }
